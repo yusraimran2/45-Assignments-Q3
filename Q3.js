bc3ef228bbc9a3114d348e5160412457c5016413
yusraimran2 <yusraimran12@hotmail.com>
2024-11-12 20:43:22 +0500

    Add files via upload

---
+++ Q3.js
@@ -0,0 +1,6 @@
+"use strict";
+//3. Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+let firstName = "yusra imran";
+console.log(firstName.toLowerCase());
+console.log(firstName.toLocaleUpperCase());
+console.log(firstName.charAt(0).toLocaleUpperCase() + firstName.slice(1));
